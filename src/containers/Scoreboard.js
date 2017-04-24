9d3aec99bb8aeea89a115cf7e95dd74eee09fa24
galvazudys <golfas705@yahoo.com>
2017-04-24 11:54:04 +0100

    moved all components to they own files

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {Components} from 'react';
 import Stopwatch from '../components/Stopwatch';
 import Stats from '../components/Stats';
 import Counter from '../components/Counter';
 import AddPlayerForm from '../components/AddPlayerForm';
+import Player from '../components/Player';
+import Header from '../components/Header';
 
 const INITIAL_STATE = {
   players: [
@@ -66,49 +68,5 @@ const Scoreboard = React.createClass({
     );
   }
 });
-
-// ----------------------------------------------
-
-function Header(props) {
-  return (
-    <div className="header">
-      <Stats players={props.players}/>
-      <h1>Scoreboard</h1>
-      <Stopwatch/>
-    </div>
-  );
-}
-
-Header.propTypes = {
-  players: React.PropTypes.array.isRequired
-};
-
-// Move to components/Stats.js
-// -----------------------------------------------------------------------
-// ------------------------------------------------------------------------
-// ----------------------------------------------------------------------
-
-function Player(props) {
-  return (
-    <div className="player">
-      <div className="player-name">
-        <a className="remove-player" onClick={props.onRemove}>✖</a>
-        {props.name}
-      </div>
-      <div className="player-score">
-        <Counter onChange={props.onScoreChange} score={props.score}/>
-      </div>
-    </div>
-  );
-}
-
-Player.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  score: React.PropTypes.number.isRequired,
-  onRemove: React.PropTypes.func.isRequired,
-  onScoreChange: React.PropTypes.func.isRequired
-};
-
-// ----------------------------------------------------------
 
 export default Scoreboard;
